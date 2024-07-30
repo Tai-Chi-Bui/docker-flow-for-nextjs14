636cf82833b9f500d6fbab9aba5f29e7d1fdad54
Tai Bui <tai.c.bui@gmail.com>
2024-07-30 16:42:54 +0700

    update

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ An app serves as benchmark for microservices technology
 
 ## When in development, run this:
 
+```docker-compose -f docker-compose.dev.yml up -d --build```
+
 to deport, run this:
 
 ```docker-compose -f docker-compose.dev.yml down```
--- host-client/next.config.mjs
+++ host-client/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // This options allow hot reload on docker window destop for this app 
     webpackDevMiddleware: config => {
         config.watchOptions = {
             poll: 1000,
